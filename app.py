c3fc215223dc2fae85d8bad217963993671c318b
vq0001 <vquiroga2023@udec.cl>
2024-11-03 18:50:19 -0300

    Crear una aplicacion principal basica

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, render_template, request, url_for
+from funciones import *
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST']) # GET is not secure, POST is secure
+def login():
+
+    if request.method == 'POST':
+        name = request.form['name-input']
+        email = request.form['email-input']
+
+        writeToFile(name, email, datetime.datetime.now())
+        
+        return redirect(url_for('user', usr=name))
+    else:
+        return render_template('login.html')
+
+@app.route('/<usr>')
+def user(usr):
+    return f'<h1>{usr}</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
