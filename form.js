e50c93643da81545511f966807d652f2427cb9ce
Lunara02 <cristobagonzalez2023@inf.udec.cl>
2024-11-11 22:23:35 -0300

    feat: cancelar, cambiar hora

--- form.js
+++ form.js
@@ -1,13 +1,29 @@
-// Espera a que el contenido del documento se haya cargado completamente
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtiene los parámetros de la URL
+document.addEventListener("DOMContentLoaded", function () {
+    const changeDateLink = document.getElementById("changeDateLink");
+    const fechaCitaInput = document.getElementById("fechaCita");
+
     const urlParams = new URLSearchParams(window.location.search);
-    // Obtiene el valor del parámetro 'fechaCita'
     const fechaCita = urlParams.get('fechaCita');
-    
-    // Si se proporciona la fechaCita, la establece en el campo correspondiente
+
     if (fechaCita) {
-        document.getElementById("fechaCita").value = fechaCita;
+        fechaCitaInput.value = fechaCita;
     }
+
+    const convert_spanish = {
+        "january": "enero", "february": "febrero", "march": "marzo", "april": "abril",
+        "may": "mayo", "june": "junio", "july": "julio", "august": "agosto",
+        "september": "septiembre", "october": "octubre", "november": "noviembre", "december": "diciembre"
+    };
+
+    changeDateLink.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const selectedDate = new Date(fechaCitaInput.value);
+
+        const monthInEnglish = selectedDate.toLocaleString('default', { month: 'long' }).toLowerCase();
+        
+        const monthInSpanish = convert_spanish[monthInEnglish];
+
+        window.location.href = `monthly_calendar.html?mes=${monthInSpanish}`;
+    });
 });
-    
